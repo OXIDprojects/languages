34093151fd6e8f3ba04a499735a7d4146daa35d6
Marco <marco.steinhaeuser@gmail.com>
2012-07-13 16:39:06 -0700

    streamlining language folders and transliteration lists

---
+++ out/de_in/translit_lang.php
@@ -0,0 +1,14 @@
+/**
+ * Character list for replacement in SEO URL's
+ * @var array SEO replacement list
+ */
+
+$aSeoReplaceChars = array(
+    'ä' => 'ae',
+    'ö' => 'oe',
+    'ü' => 'ue',
+    'Ä' => 'AE',
+    'Ö' => 'OE',
+    'Ü' => 'UE',
+    'ß' => 'ss',
+);
