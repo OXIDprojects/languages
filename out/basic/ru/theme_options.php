a0eba09503ebe2e301e62fcf148ea8c47b6db4eb
Marco <marco.steinhaeuser@gmail.com>
2012-08-23 00:29:01 -0700

    Language pack update.

--- out/basic/ru/theme_options.php
+++ out/basic/ru/theme_options.php
@@ -19,7 +19,7 @@
  * @package   lang
  * @copyright (C) OXID eSales AG 2003-2012
  * @version OXID eShop CE
- * @translators: marco (98), pianoservis (1), vanilla.thunder (35)
+ * @translators: marco (824), pianoservis (1), unicum (29), vanilla.thunder (35)
  */
 
 $sLangName  = "Русский";
