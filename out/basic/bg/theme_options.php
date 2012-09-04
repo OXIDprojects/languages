4c1da94ba226c02403e7291e6f13d7d98f94dec9
Marco <marco.steinhaeuser@gmail.com>
2012-09-04 00:58:03 -0700

    Language pack update.

--- out/basic/bg/theme_options.php
+++ out/basic/bg/theme_options.php
@@ -19,7 +19,7 @@
  * @package   lang
  * @copyright (C) OXID eSales AG 2003-2012
  * @version OXID eShop CE
- * @translators: crashover (26)
+ * @translators: crashover (191)
  */
 
 $sLangName  = "Български";
