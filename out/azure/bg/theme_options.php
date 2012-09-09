e6f78bbfa1e4944b7114d1d77cff0ec64e5f750f
Marco <marco.steinhaeuser@gmail.com>
2012-09-09 10:56:14 -0700

    Language pack update.

--- out/azure/bg/theme_options.php
+++ out/azure/bg/theme_options.php
@@ -19,7 +19,7 @@
  * @package   lang
  * @copyright (C) OXID eSales AG 2003-2012
  * @version OXID eShop CE
- * @translators: crashover (311)
+ * @translators: crashover (328)
  */
 
 $sLangName  = "Български";
