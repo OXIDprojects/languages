15127c656790d06e559aecad2e82826ab357626e
Marco Steinhaeuser <marco.steinhaeuser@gmail.com>
2013-05-03 10:28:00 +0200

    added da lang flag and translit list

---
+++ application/translations/da/translit_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Character list for replacement in SEO URL's
+ * @var array SEO replacement list
+**/
+
+$aSeoReplaceChars = array(
+'Å' => 'A',
+'Æ' => 'AE',
+'É' => 'E',
+'Ø' => 'O',
+'Ü' => 'UE',
+'å' => 'a',
+'æ' => 'ae',
+'é' => 'e',
+'ø' => 'o',
+'ü' => 'ue',
+);
