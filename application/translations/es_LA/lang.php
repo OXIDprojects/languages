dfb6c359c42e5f11d3d07330b4e6ec247652e80a
Jordhy Ledesma <jv@jordhy.com>
2014-09-15 09:27:30 +0200

    Language pack update.

--- application/translations/es_LA/lang.php
+++ application/translations/es_LA/lang.php
@@ -19,7 +19,7 @@
  * @package   lang
  * @copyright (C) OXID eSales AG 2003-2013
  * @version OXID eShop CE
- * @translators: jordhy (917), jsmaster (11)
+ * @translators: jordhy (3158), jsmaster (11)
  */
 
 $sLangName  = 'Español de América';
