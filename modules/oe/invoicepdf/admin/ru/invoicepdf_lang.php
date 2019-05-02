e397f3d1f2a914210d5d85f1c9620ea22aa97195
kermie <info@marco-steinhaeuser.de>
2019-05-02 15:57:08 +0200

    added UK and RU lang files for invoicepdf module @pianoservis

---
+++ modules/oe/invoicepdf/admin/ru/invoicepdf_lang.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file is part of OXID eSales Invoice PDF module.
+ *
+ * OXID eSales Invoice PDF module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Invoice PDF module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Invoice PDF module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category      module
+ * @package       oeinvoicepdf
+ * @author        OXID eSales AG
+ * @link          http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+/*
+ * Capitalisation in this document:
+ * First letter is always capitalized
+ * All nouns are capitalized
+ */
+$sLangName  = "Русский";
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset'                               => 'UTF-8',
+    'ORDER_OVERVIEW_PDF_ALLPRICENETTO'      => 'Сумма (netto)',
+    'ORDER_OVERVIEW_PDF_ZZGLVAT'            => 'zzgl. MwSt. ',
+    'ORDER_OVERVIEW_PDF_PERCENTSUM'         => '% Betrag',
+    'ORDER_OVERVIEW_PDF_ALLPRICEBRUTTO'     => 'Сумма (brutto)',
+    'ORDER_OVERVIEW_PDF_DISCOUNT'           => 'Скидка',
+    'ORDER_OVERVIEW_PDF_VOUCHER'            => 'Купон',
+    'ORDER_OVERVIEW_PDF_SHIPCOST'           => 'Стоимость доставки',
+    'ORDER_OVERVIEW_PDF_NETTO'              => '(netto)',
+    'TOTAL_PLUS_PROPORTIONAL_VAT'           => 'plus MwSt. (anteilig berechnet)',
+    'ORDER_OVERVIEW_PDF_BRUTTO'             => '(brutto)',
+    'WRAPPING_COSTS'                        => 'Подарочная упаковка',
+    'GIFTCARD_COSTS'                        => 'Поздравительная открытка',
+    'ORDER_OVERVIEW_PDF_PAYMENTIMPACT'      => 'Способ оплаты',
+    'ORDER_OVERVIEW_PDF_ALLSUM'             => 'Всего (brutto)',
+    'ORDER_OVERVIEW_PDF_SELPAYMENT'         => 'Способ оплаты: ',
+    'ORDER_OVERVIEW_PDF_PAYUPTO'            => 'Оплатите, пожалуйста до ',
+    'ORDER_OVERVIEW_PDF_PHONE'              => 'Телефон: ',
+    'ORDER_OVERVIEW_PDF_FAX'                => 'Факс: ',
+    'ORDER_OVERVIEW_PDF_ACCOUNTNR'          => 'Номер счета/IBAN: ',
+    'ORDER_OVERVIEW_PDF_BANKCODE'           => 'МФО (oder BIC): ',
+    'ORDER_OVERVIEW_PDF_VATID'              => 'ЕГРПОУ/ИНН: ',
+    'ORDER_OVERVIEW_PDF_TAXID'              => 'Steuernr.: ',
+    'ORDER_OVERVIEW_PDF_AMOUNT'             => 'Кол-во',
+    'ORDER_OVERVIEW_PDF_ARTID'              => 'Арт. №',
+    'ORDER_OVERVIEW_PDF_DESC'               => 'Описание',
+    'ORDER_OVERVIEW_PDF_UNITPRICE'          => 'Цена за единицу',
+    'ORDER_OVERVIEW_PDF_ALLPRICE'           => 'Итого',
+    'ORDER_OVERVIEW_PDF_DELIVERYADDRESS'    => 'Адрес доставки',
+    'ORDER_OVERVIEW_PDF_FILLONPAYMENT'      => 'Укажите при оплате',
+    'ORDER_OVERVIEW_PDF_CUSTNR'             => 'Клиентский номер',
+    'ORDER_OVERVIEW_PDF_TAXIDNR'            => 'ИНН:',
+    'ORDER_OVERVIEW_PDF_COUNTNR'            => 'Счет №:',
+    'ORDER_OVERVIEW_PDF_STORNO'             => 'ОТМЕНА',
+    'ORDER_OVERVIEW_PDF_PURCHASENR'         => 'Заказ No',
+    'ORDER_OVERVIEW_PDF_ORDERSFROM'         => 'Ваш заказ ',
+    'ORDER_OVERVIEW_PDF_ORDERSAT'           => ' на ',
+    'ORDER_OVERVIEW_PDF_VAT'                => 'НДС %',
+    'ORDER_OVERVIEW_PDF_GREETINGS'          => 'Спасибо за ваш заказ! Будем рады снова видеть Вас в нашем магазине. :)',
+    'ORDER_OVERVIEW_PDF_DELIVNOTE'          => 'Номер заказа',
+);
---
+++ modules/oe/invoicepdf/admin/uk/invoicepdf_lang.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file is part of OXID eSales Invoice PDF module.
+ *
+ * OXID eSales Invoice PDF module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Invoice PDF module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Invoice PDF module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category      module
+ * @package       oeinvoicepdf
+ * @author        OXID eSales AG
+ * @link          http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+/*
+ * Capitalisation in this document:
+ * First letter is always capitalized
+ * All nouns are capitalized
+ */
+$sLangName  = "Українська";
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset'                               => 'UTF-8',
+    'ORDER_OVERVIEW_PDF_ALLPRICENETTO'      => 'Сума (netto)',
+    'ORDER_OVERVIEW_PDF_ZZGLVAT'            => 'zzgl. MwSt. ',
+    'ORDER_OVERVIEW_PDF_PERCENTSUM'         => '% Betrag',
+    'ORDER_OVERVIEW_PDF_ALLPRICEBRUTTO'     => 'Сума (brutto)',
+    'ORDER_OVERVIEW_PDF_DISCOUNT'           => 'Знижка',
+    'ORDER_OVERVIEW_PDF_VOUCHER'            => 'Купон',
+    'ORDER_OVERVIEW_PDF_SHIPCOST'           => 'Вартість доставки',
+    'ORDER_OVERVIEW_PDF_NETTO'              => '(netto)',
+    'TOTAL_PLUS_PROPORTIONAL_VAT'           => 'plus MwSt. (anteilig berechnet)',
+    'ORDER_OVERVIEW_PDF_BRUTTO'             => '(brutto)',
+    'WRAPPING_COSTS'                        => 'Подарункове пакування',
+    'GIFTCARD_COSTS'                        => 'Вітальна листіва',
+    'ORDER_OVERVIEW_PDF_PAYMENTIMPACT'      => 'Метод оплати',
+    'ORDER_OVERVIEW_PDF_ALLSUM'             => 'Всього (brutto)',
+    'ORDER_OVERVIEW_PDF_SELPAYMENT'         => 'Спосіб оплати: ',
+    'ORDER_OVERVIEW_PDF_PAYUPTO'            => 'Сплатіть, будь ласка до ',
+    'ORDER_OVERVIEW_PDF_PHONE'              => 'Телефон: ',
+    'ORDER_OVERVIEW_PDF_FAX'                => 'Fax: ',
+    'ORDER_OVERVIEW_PDF_ACCOUNTNR'          => 'Рахунок/IBAN: ',
+    'ORDER_OVERVIEW_PDF_BANKCODE'           => 'МФО (oder BIC): ',
+    'ORDER_OVERVIEW_PDF_VATID'              => 'ЄДРПОУ/ІПН: ',
+    'ORDER_OVERVIEW_PDF_TAXID'              => 'Steuernr.: ',
+    'ORDER_OVERVIEW_PDF_AMOUNT'             => 'Кількість',
+    'ORDER_OVERVIEW_PDF_ARTID'              => 'Арт.-Nr.',
+    'ORDER_OVERVIEW_PDF_DESC'               => 'Найменування',
+    'ORDER_OVERVIEW_PDF_UNITPRICE'          => 'Ціна за одиницю',
+    'ORDER_OVERVIEW_PDF_ALLPRICE'           => 'Разом',
+    'ORDER_OVERVIEW_PDF_DELIVERYADDRESS'    => 'Адреса доставки',
+    'ORDER_OVERVIEW_PDF_FILLONPAYMENT'      => 'Вкажіть при оплаті',
+    'ORDER_OVERVIEW_PDF_CUSTNR'             => 'Клієнтський номер:',
+    'ORDER_OVERVIEW_PDF_TAXIDNR'            => 'ІПН',
+    'ORDER_OVERVIEW_PDF_COUNTNR'            => 'Рахунок №:',
+    'ORDER_OVERVIEW_PDF_STORNO'             => 'СКАСУВАННЯ',
+    'ORDER_OVERVIEW_PDF_PURCHASENR'         => 'Замовлення №:',
+    'ORDER_OVERVIEW_PDF_ORDERSFROM'         => 'Ваше замовлення ',
+    'ORDER_OVERVIEW_PDF_ORDERSAT'           => ' у ',
+    'ORDER_OVERVIEW_PDF_VAT'                => 'ПДВ %',
+    'ORDER_OVERVIEW_PDF_GREETINGS'          => 'Дякуємо за Ваше замовлення! Будемо раді знову бачити Вас в нашому магазині. :)',
+    'ORDER_OVERVIEW_PDF_DELIVNOTE'          => 'Замовлення №',
+);
